7debb11202142a3f4446d45535b594392674b3a1
julio <jspairani@gmail.com>
2015-05-28 17:00:54 -0300

    cambios

--- ml/src/test/java/ml/tp1/WekaJ48ScriptsTP1.java
+++ ml/src/test/java/ml/tp1/WekaJ48ScriptsTP1.java
@@ -21,9 +21,79 @@ import weka.classifiers.trees.J48;
 import weka.core.Debug.Random;
 import weka.core.Instances;
 import weka.core.converters.ArffLoader;
+import weka.core.converters.ArffSaver;
+import weka.core.converters.CSVLoader;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.NumericToNominal;
+import weka.filters.unsupervised.attribute.Remove;
 
 public class WekaJ48ScriptsTP1 {
 
+	
+	@Test
+	public void filtersNewFile() {
+		final String encode = "UTF-8";
+		// por los acentos
+		System.setProperty("file.encoding", encode);
+
+		//String pathDatos = "C:/julio/dev/R/";
+		String pathDatos ="C:/Users/jspairani/Dropbox/julio_box/educacion/maestria_explotacion_datos_uba/materias/cuat_1_aprendizaje_automatico/TP1/rworkdir/";
+		String datosName = "20150526_0501_2013_y_2014-refined_v3.txt";
+		
+		String dummy = "dummy.arff";
+		String outReal = "20150526_0501_2013_y_2014-refined_v3.arff";
+
+		//String exportResult = "export_corrida.txt";
+		
+		try {
+			// levantar el original
+			CSVLoader loader = new CSVLoader();
+			loader.setSource(new File(pathDatos + datosName));
+			Instances oldData = loader.getDataSet();
+
+			// filtro de numericos
+			NumericToNominal numToNom = new NumericToNominal(); 
+			numToNom.setDebug(false);
+			numToNom.setInvertSelection(false);
+			int[] indeces =  {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,16};
+			numToNom.setAttributeIndicesArray(indeces);
+			numToNom.setInputFormat(oldData);
+
+			// archivo filtrado
+			Instances data = Filter.useFilter(oldData, numToNom);
+			
+			// filtro de sacar columna
+			
+//			 String[] options = new String[2];
+//			 options[0] = "-R";
+//			 options[1] = "15";
+//			 
+//			 Remove remove = new Remove();
+//			 remove.setOptions(options);                           // set options
+//			 remove.setInputFormat(data);                          // inform filter about dataset **AFTER** setting options
+//			 Instances newData = Filter.useFilter(data, remove);   // apply filter
+//			
+			
+			ArffSaver saver = new ArffSaver();
+			saver.setInstances(data);
+			
+			// agregado por el error de " Cannot create a new output file. Standard out is used."
+			File outFile = new File(pathDatos+dummy);
+			File outFile2 = new File(pathDatos+outReal);
+//		outFile2.set
+			
+			saver.setFile(outFile);
+			saver.setDestination(outFile2);
+			saver.writeBatch();
+			Assert.assertTrue(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+			Assert.fail("revento : "+e.getMessage());
+		}
+		
+		
+	}
+	
 	@Test
 	public void correrJ48Prueba() {
 		final String encode = "utf-8";
@@ -38,8 +108,27 @@ public class WekaJ48ScriptsTP1 {
 		try {
 			ArffLoader loader = new ArffLoader();
 			loader.setSource(new File(pathDatos + datosName));
-			Instances data = loader.getDataSet();
+			Instances oldData = loader.getDataSet();
 
+			
+			NumericToNominal numToNom = new NumericToNominal(); 
+			numToNom.setDebug(false);
+			numToNom.setInvertSelection(false);
+			int[] indeces =  {2,3,4,5,6,7,8,9};
+			numToNom.setAttributeIndicesArray(indeces);
+			
+//			@attribute cod_cuenta_1 {}
+//			@attribute cod_cuenta_2 numeric
+//			@attribute cod_cuenta_3 numeric
+//			@attribute cod_cuenta_4 numeric
+//			@attribute cod_cuenta_5 numeric
+//			@attribute cod_cuenta_6 numeric
+//			@attribute cod_cuenta_7 numeric
+//			@attribute cod_cuenta_8 numeric
+			
+			Instances data = Filter.useFilter(oldData, numToNom);
+			
+			
 			//
 			// seteo el indice de la columna que va a ser clasificador
 			// data.setClassIndex(data.numAttributes() - 1);
