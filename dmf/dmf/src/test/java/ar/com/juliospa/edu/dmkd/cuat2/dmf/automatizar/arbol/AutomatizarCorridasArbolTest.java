5c81dd55c1979ee6b152d45b49c235212f8199aa
julio <jspairani@gmail.com>
2015-10-16 16:21:43 -0300

    mas spss

--- dmf/dmf/src/test/java/ar/com/juliospa/edu/dmkd/cuat2/dmf/automatizar/arbol/AutomatizarCorridasArbolTest.java
+++ dmf/dmf/src/test/java/ar/com/juliospa/edu/dmkd/cuat2/dmf/automatizar/arbol/AutomatizarCorridasArbolTest.java
@@ -3,6 +3,7 @@ package ar.com.juliospa.edu.dmkd.cuat2.dmf.automatizar.arbol;
 import org.junit.Test;
 
 import ar.com.juliospa.edu.dmkd.cuat1.dmf.automatizar.arbol.AutomatizarCorridasArbol;
+import ar.com.juliospa.edu.dmkd.cuat1.dmf.automatizar.arbol.AutomatizarCorridasArbolJulio;
 
 public class AutomatizarCorridasArbolTest {
 
@@ -14,6 +15,11 @@ public class AutomatizarCorridasArbolTest {
 	@Test
 	public void pruebaEjecucionSpssArbolVersion2() {
 		AutomatizarCorridasArbol.pruebaEjecucionSpssArbolVersion2();
+	}
+	
+	@Test
+	public void pruebaEjecucionSpssArbolVersionJulio() {
+		AutomatizarCorridasArbolJulio.pruebaEjecucionSpssArbolVersion2();
 		
 	}
 
