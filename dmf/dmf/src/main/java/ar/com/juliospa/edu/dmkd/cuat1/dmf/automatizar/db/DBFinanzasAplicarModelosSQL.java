7333a18b5ba7a8ff1b666692e29a540338de7a3f
julio <jspairani@gmail.com>
2015-11-12 17:06:49 -0300

    para automatizar el coso de sql y rpobabilidades

---
+++ dmf/dmf/src/main/java/ar/com/juliospa/edu/dmkd/cuat1/dmf/automatizar/db/DBFinanzasAplicarModelosSQL.java
@@ -0,0 +1,45 @@
+package ar.com.juliospa.edu.dmkd.cuat1.dmf.automatizar.db;
+
+import java.util.List;
+
+public class DBFinanzasAplicarModelosSQL {
+
+	
+	private List<String> modelosSQL;
+	
+	public DBFinanzasAplicarModelosSQL() {}
+	
+	
+	public void aplicarModelosBase(){
+	
+		
+		
+	}
+	
+	public void gatherNodos(){
+		
+	}
+	
+	
+	
+	
+
+	public List<String> getModelosSQL() {
+		return modelosSQL;
+	}
+
+	/**
+	 * para setear todos las carpetas con modelos a evaluar
+	 * los archivos que se tendran en cuenta seran los que tienen
+	 * reglas.sql
+	 * 
+	 * @param modelosSQL
+	 */
+	public void setModelosSQL(List<String> modelosSQL) {
+		this.modelosSQL = modelosSQL;
+	}
+	
+
+	
+	
+}
